c1d0c1ffbb8161edb576cec447e55dc5fb56a051
Syd Bailey <sydabailey@gmail.com>
2019-08-06 03:52:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,152 @@
-# Write your code here!
+require "pry"
 
+def game_hash
+  game = {home:
+                {team_name: "Brooklyn Nets",
+                colors: ["Black", "White"],
+                players: [{
+                  name: "Alan Anderson",
+                  number: 0,
+                  shoe: 16,
+                  points: 22,
+                  rebounds: 12,
+                  assists: 12,
+                  steals: 3,
+                  blocks: 1,
+                  slam_dunks: 1
+                          },
+                          {
+                  name: "Reggie Evans",
+                  number: 30,
+                  shoe: 14,
+                  points: 12,
+                  rebounds: 12,
+                  assists: 12,
+                  steals: 12,
+                  blocks: 12,
+                  slam_dunks: 7
+                          },
+                          {
+                  name: "Brook Lopez",
+                  number: 11,
+                  shoe: 17,
+                  points: 17,
+                  rebounds: 19,
+                  assists: 10,
+                  steals: 3,
+                  blocks: 1,
+                  slam_dunks: 15
+                          },
+                          {
+                  name: "Mason Plumlee",
+                  number: 1,
+                  shoe: 19,
+                  points: 26,
+                  rebounds: 11,
+                  assists: 6,
+                  steals: 3,
+                  blocks: 8,
+                  slam_dunks: 5
+                          },
+                          {
+                  name: "Jason Terry",
+                  number: 31,
+                  shoe: 15,
+                  points: 19,
+                  rebounds: 2,
+                  assists: 2,
+                  steals: 4,
+                  blocks: 11,
+                  slam_dunks: 1
+                          }
+                          ]
+              },
+             away:
+                {team_name: "Charlotte Hornets",
+                colors: ["Turquoise", "Purple"],
+                players: [
+                          {
+                  name: "Jeff Adrien",
+                  number: 4,
+                  shoe: 18,
+                  points: 10,
+                  rebounds: 1,
+                  assists: 1,
+                  steals: 2,
+                  blocks: 7,
+                  slam_dunks: 2
+                          },
+                          {
+                  name: "Bismack Biyombo",
+                  number: 0,
+                  shoe: 16,
+                  points: 12,
+                  rebounds: 4,
+                  assists: 7,
+                  steals: 22,
+                  blocks: 15,
+                  slam_dunks: 10
+                          },
+                          {
+                  name: "DeSagna Diop",
+                  number: 2,
+                  shoe: 14,
+                  points: 24,
+                  rebounds: 12,
+                  assists: 12,
+                  steals: 4,
+                  blocks: 5,
+                  slam_dunks: 5
+                          },
+                          {
+                  name: "Ben Gordon",
+                  number: 8,
+                  shoe: 15,
+                  points: 33,
+                  rebounds: 3,
+                  assists: 2,
+                  steals: 1,
+                  blocks: 1,
+                  slam_dunks: 0
+                          },
+                          {
+                  name: "Kemba Walker",
+                  number: 33,
+                  shoe: 15,
+                  points: 6,
+                  rebounds: 12,
+                  assists: 12,
+                  steals: 7,
+                  blocks: 5,
+                  slam_dunks: 12
+                          }
+                  ]
+                }
+             }
+end
 
-
-
-
-
-
-
-
-
+def num_points_scored(players_name)
+  home_team = game_hash[:home]
+  away_team = game_hash[:away]
+  
+  home_team_players = home_team[:players]
+  away_team_players = away_team[:players]
+  
+  alan_anderson = home_team_players[0]
+  alan_anderson_shoe_size = alan_anderson[:shoe]
+  
+  
+  binding.pry
+  # get a list of players!
+  game_hash
+  #call on game_hash
+  
+  #iterate (call on) game_hash and return an array (list) of players
+  
+  # iterate through the list of players to find a player who's name matches the name entered
+  #return that players' points! 
+  
+  game_hash.each do |team_location, team_info|
+    if team_info.include?(players_name)
+      return game_hash[team_location][team_info][players_name]
+    
